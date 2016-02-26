cc8a8b42131da2972c8b46a935747477f8ee018e
Zhe Wang <0x1998@gmail.com>
2016-02-26 13:18:28 +0800

    Use empty password when initializing the system database

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -25,7 +25,7 @@ if [ "$1" = 'mysqld' ]; then
 		chown -R mysql:mysql "$DATADIR"
 
 		echo 'Initializing database'
-		mysqld --initialize --user=mysql --datadir="$DATADIR"
+		mysqld --initialize-insecure --user=mysql --datadir="$DATADIR"
 		echo 'Database initialized'
 
 		"$@" --skip-networking &
